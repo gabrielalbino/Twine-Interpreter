948c8a34f221591535fcd977acc7c6c03076fa4b
Gabriel Albino <enggabrielalbino@gmail.com>
2022-03-09 21:57:43 -0300

    Initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,85 @@
+from types import SimpleNamespace
+import pytest
+import sys
+import pathlib
+import os
+
+TEST_PATH = pathlib.Path(__file__).parent
+PROJECT_PATH = TEST_PATH.parent
+SRC_PATH = PROJECT_PATH / "twine"
+
+sys.path.append(".")
+sys.path.append(str(PROJECT_PATH))
+
+
+@pytest.fixture(scope="session")
+def twine():
+    if version := os.environ.get("USE", ""):
+        root = str(PROJECT_PATH / ("_" + version))
+        if sys.path[0] != root:
+            sys.path.insert(0, root)
+
+    import twine
+
+    return twine
+
+
+@pytest.fixture(scope="session")
+def lex(twine):
+    from twine.lexer import lex
+
+    return lambda src: list(lex(src))
+
+
+@pytest.fixture(scope="session")
+def parse(twine):
+    from twine.parser import parse
+
+    return parse
+
+
+@pytest.fixture(scope="session")
+def ir(twine):
+    from twine.parser import parse
+    from twine.ir import transform
+
+    return lambda src: transform(parse(src))
+
+
+@pytest.fixture(scope="session")
+def eval(twine):
+    from twine.interpreter import eval
+
+    return eval
+
+
+
+@pytest.fixture
+def env(twine):
+    from twine.interpreter import default_env
+
+    return default_env()
+
+@pytest.fixture(scope="session")
+def spec():
+    return SimpleNamespace(
+        keywords={
+            "integer",
+            "boolean",
+            "true",
+            "false",
+            "if",
+            "else",
+            "f",
+            "returns",
+            "print",
+        },
+        operators=[*"+-*/<=^|~"],
+        punctuation=[*"():,"],
+        special_characters=[*"():,+-*/<=^|~%$_"],
+    )
+
+
+@pytest.fixture(scope="session")
+def examples():
+    return PROJECT_PATH / "exemplos"
